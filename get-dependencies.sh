eed5361414a6082f40cdfbca45967023fafe3a52
ArqamQazi <arqamqazi549@gmail.com>
2026-09-04 12:39:39 +0530

    Extract package in /tmp and preserve workspace directory permissions

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -47,16 +47,22 @@ esac
 
 CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_${deb_arch}.deb"
 
-mkdir -p ./AppDir/bin
-wget --retry-connrefused --tries=30 "$CHROME_URL"
-ar xvf ./google-chrome-stable_current_${deb_arch}.deb
+mkdir -p ./AppDir/bin /tmp/chrome
+wget --retry-connrefused --tries=30 "$CHROME_URL" -O /tmp/chrome/chrome.deb
+
+cd /tmp/chrome
+ar xvf ./chrome.deb
 tar xvf ./data.tar.xz
 
 mkdir -p /usr/share/applications /usr/share/icons/hicolor/256x256/apps
 cp -v ./usr/share/applications/google-chrome.desktop /usr/share/applications/google-chrome.desktop
 cp -v ./opt/google/chrome/product_logo_256.png /usr/share/icons/hicolor/256x256/apps/google-chrome.png
 
-mv -v ./opt/google/chrome/* ./AppDir/bin
+tar -xf ./control.tar.* ./control -O | awk -F': |-' '/^Version:/{print $2; exit}' > ~/version
+
+cd -
+mv -v /tmp/chrome/opt/google/chrome/* ./AppDir/bin
+rm -rf /tmp/chrome
 
 # Symlink so the desktop entry's Exec command (google-chrome-stable) resolves to chrome
 ln -sf chrome ./AppDir/bin/google-chrome-stable
@@ -65,8 +71,7 @@ ln -sf chrome ./AppDir/bin/google-chrome-stable
 # when present, we can only bunle libqt6 or libqt5 but not both
 rm -f ./AppDir/bin/libqt5_shim.so
 
-tar -xf ./control.tar.* ./control -O | awk -F': |-' '/^Version:/{print $2; exit}' > ~/version
-rm -rf ./usr ./opt ./etc ./*.deb ./*.tar.* debian-binary
+chmod 755 .
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
